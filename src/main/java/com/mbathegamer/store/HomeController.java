3979a643a9f6ed9ccfd2a8940d5e8692cd6c5f31
MBATheGamer <mbathegamer@gmail.com>
2025-08-04 14:33:39 +0100

    Configure application properties

--- src/main/java/com/mbathegamer/store/HomeController.java
+++ src/main/java/com/mbathegamer/store/HomeController.java
@@ -1,10 +1,17 @@
 package com.mbathegamer.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @Value("${app.page-size}")
+  private int pageSize;
+
   @GetMapping("/")
   public String index() {
     // String viewName = getViewName();
@@ -14,7 +21,7 @@ public class HomeController {
     return "index.html";
   }
 
-  private String getViewName() {
-    return "index";
-  }
+  // private String getViewName() {
+  // return "index";
+  // }
 }
