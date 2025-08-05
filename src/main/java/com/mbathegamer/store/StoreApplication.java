6cfc875a9e54290f78e619ffd997dd5caa0e056e
MBATheGamer <mbathegamer@gmail.com>
2025-08-05 22:10:08 +0100

    Learn `Setter Injection` concept

--- src/main/java/com/mbathegamer/store/StoreApplication.java
+++ src/main/java/com/mbathegamer/store/StoreApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
-	public static void main(String[] args) {
-		// SpringApplication.run(StoreApplication.class, args);
-		var orderService = new OrderService(new StripePaymentService());
-		orderService.placeOrder();
+  public static void main(String[] args) {
+    // SpringApplication.run(StoreApplication.class, args);
+    var orderService = new OrderService();
+    orderService.setPaymentService(new StripePaymentService());
+    orderService.placeOrder();
 
-		orderService = new OrderService(new PayPalPaymentService());
-		orderService.placeOrder();
-	}
+    // orderService = new OrderService(new PayPalPaymentService());
+    orderService.setPaymentService(new PayPalPaymentService());
+    orderService.placeOrder();
+  }
 }
