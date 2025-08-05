de2b8f750241cf6981f5881fce1e1b83a11a36c7
MBATheGamer <mbathegamer@gmail.com>
2025-08-05 22:29:14 +0100

    Control `Bean` selection

--- src/main/java/com/mbathegamer/store/OrderService.java
+++ src/main/java/com/mbathegamer/store/OrderService.java
@@ -1,17 +1,17 @@
 package com.mbathegamer.store;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
   private PaymentService paymentService;
 
-  public OrderService() {
-  }
+  // public OrderService() {
+  // }
 
-  @Autowired
-  public OrderService(PaymentService paymentService) {
+  // @Autowired
+  public OrderService(@Qualifier("paypal") PaymentService paymentService) {
     this.paymentService = paymentService;
   }
 
