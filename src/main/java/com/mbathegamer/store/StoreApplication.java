38fc7c45dc013560427c9fcb2c8224480e0979b1
MBATheGamer <mbathegamer@gmail.com>
2025-08-05 22:19:03 +0100

    Learn `Spring IoC Container` concept

--- src/main/java/com/mbathegamer/store/StoreApplication.java
+++ src/main/java/com/mbathegamer/store/StoreApplication.java
@@ -1,17 +1,23 @@
 package com.mbathegamer.store;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
   public static void main(String[] args) {
-    // SpringApplication.run(StoreApplication.class, args);
-    var orderService = new OrderService();
-    orderService.setPaymentService(new StripePaymentService());
+    ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+
+    var orderService = context.getBean(OrderService.class);
+    orderService.placeOrder();
+
+    // var orderService = new OrderService();
+    // orderService.setPaymentService(new StripePaymentService());
     orderService.placeOrder();
 
     // orderService = new OrderService(new PayPalPaymentService());
-    orderService.setPaymentService(new PayPalPaymentService());
-    orderService.placeOrder();
+    // orderService.setPaymentService(new PayPalPaymentService());
+    // orderService.placeOrder();
   }
 }
