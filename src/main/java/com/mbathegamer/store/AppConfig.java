b28937951ce07352cd27cd22a39719191dde3f44
MBATheGamer <mbathegamer@gmail.com>
2025-08-06 21:47:55 +0100

    Exercise: Add payment gateway

--- src/main/java/com/mbathegamer/store/AppConfig.java
+++ src/main/java/com/mbathegamer/store/AppConfig.java
@@ -1,10 +1,14 @@
 package com.mbathegamer.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+  @Value("${payment-gateway:stripe}")
+  private String paymentGateaway;
+
   @Bean
   PaymentService stripe() {
     return new StripePaymentService();
@@ -17,6 +21,10 @@ public class AppConfig {
 
   @Bean
   OrderService orderService() {
-    return new OrderService(stripe());
+    if (paymentGateaway.equals("stripe")) {
+      return new OrderService(stripe());
+    } else {
+      return new OrderService(paypal());
+    }
   }
 }
