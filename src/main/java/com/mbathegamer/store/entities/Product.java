ec1f8a3be5c147bcc902b093178f919ebbe48a16
MBATheGamer <mbathegamer@gmail.com>
2025-08-14 11:59:03 +0100

    Exercise: create `Category` and `Product` entities

---
+++ src/main/java/com/mbathegamer/store/entities/Product.java
@@ -0,0 +1,35 @@
+package com.mbathegamer.store.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+  @Id
+  @Column(name = "id", nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "price", nullable = false)
+  private BigDecimal price;
+
+  @ManyToOne()
+  @JoinColumn(name = "category_id", nullable = false)
+  private Category category;
+}
