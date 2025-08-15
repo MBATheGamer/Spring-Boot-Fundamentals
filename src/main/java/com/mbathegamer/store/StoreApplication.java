892967c02b9e079cf3e9c6ce78a663b7f104d47c
MBATheGamer <mbathegamer@gmail.com>
2025-08-15 15:12:45 +0100

    Generate database tables with `hibernate`

--- src/main/java/com/mbathegamer/store/StoreApplication.java
+++ src/main/java/com/mbathegamer/store/StoreApplication.java
@@ -1,30 +1,14 @@
 package com.mbathegamer.store;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.mbathegamer.store.entities.Profile;
-import com.mbathegamer.store.entities.User;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
   public static void main(String[] args) {
-    // ApplicationContext context = SpringApplication.run(
-    // StoreApplication.class,
-    // args);
-
-    var user = User.builder()
-        .name("John")
-        .email("john@gmail.com")
-        .password("password")
-        .build();
-
-    var profile = Profile.builder()
-        .bio("bio")
-        .build();
-
-    user.setProfile(profile);
-    profile.setUser(user);
-
-    System.out.println(user);
+    ApplicationContext context = SpringApplication.run(
+        StoreApplication.class,
+        args);
   }
 }
